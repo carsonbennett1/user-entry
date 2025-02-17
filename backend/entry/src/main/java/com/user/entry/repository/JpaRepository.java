d0c8a0afff43a0af4dc959b5bc7cbe4ad2b23259
Carson Bennett <carben2004@gmail.com>
2025-02-16 16:13:44 -0800

    Created UserRepo class

---
+++ backend/entry/src/main/java/com/user/entry/repository/JpaRepository.java
@@ -0,0 +1,5 @@
+package com.user.entry.repository;
+
+public class JpaRepository<T1, T2> {
+
+}
---
+++ backend/entry/src/main/java/com/user/entry/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.user.entry.repository;
+
+import com.user.entry.model.UserModel;
+
+public class UserRepository extends JpaRepository<UserModel, Long>{
+
+    
+    
+}
